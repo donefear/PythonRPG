f26302d3c129b4ac337785a45f6c0a297b6b5258
DoneFear <psycho.freak91@gmail.com>
2018-04-18 22:39:27 +0200

    working duel and exp gain

--- functions.py
+++ functions.py
@@ -1,43 +1,15 @@
-import discord
-from discord.ext.commands import bot
-from discord.ext import commands
 import asyncio
 import mysql.connector
 import time
 import random
 from time import gmtime, strftime
 cdate = strftime("GMT %m/%d/%Y", gmtime())
-Client = discord.Client()
-bot = commands.Bot(command_prefix = "$")
-bot.get_all_emojis()
-user = discord.User()
-
 #Connecting to DB
 cnx = mysql.connector.connect(user='bot', password='potato',database='rpg',host='127.0.0.1')
 cursor = cnx.cursor()
 
-async def duel(message,challenger,target):
-	AID = 0
-	AName = ""
-	ALevel = 0
-	AExp = 0
-	AHp = 0
-	AMaxHp = 0
-	AConst = 0
-	AStr = 0
-	AIntel = 0
-	ADex = 0
-	DID = 0
-	DName = ""
-	DLevel = 0
-	DExp = 0
-	DHp = 0
-	DMaxHp = 0
-	DConst = 0
-	DStr = 0
-	DIntel = 0
-	DDex = 0
-
+async def duel(message,challenger,target,channelid,bot):
+	print(str(challenger) + str(target))
 	print("FIGHT")
 	sqlA = "SELECT * FROM stats "" WHERE name = '%s'" % (challenger)		
 	cursor.execute(sqlA)		
@@ -71,12 +43,66 @@ async def duel(message,challenger,target):
 		DIntel = row[8]
 		DDex = row[9]	
 
-	print(AName + DName)
 	await asyncio.sleep(2)
 	AInfo = (AName, ALevel, AExp, AHp, AMaxHp, AConst, AStr, AIntel, ADex)
-	print(AInfo)
 	DInfo = (DName, DLevel, DExp, DHp, DMaxHp, DConst, DStr, DIntel, DDex)
-	print(DInfo)
-	winner = "⚔EVERYONE LOSES⚔"
+
+
+	coinwinner = random.randint(0,1)
+	if coinwinner == 0:
+		await bot.send_message(channelid, "Winner of the CoinFlip is %s they get the first strike" % (AName))
+	else:
+		await bot.send_message(channelid, "Winner of the CoinFlip is %s they get the first strike" % (DName))
+	msg  = await bot.send_message(channelid, "%s 🗡 Remaining HP : %s \n %s 🛡 Remaining HP : %s" % (AName,AHp , DName, DHp))
+	n = 1
+	while AHp >= 0 & DHp >=0 :	
+			
+		if coinwinner == 0 :
+			AHp = combat(AInfo , DInfo)
+			coinwinner = 1
+			AInfo = (AName, ALevel, AExp, AHp, AMaxHp, AConst, AStr, AIntel, ADex)
+			n = n+1
+		else:
+			DHp = combat(DInfo , AInfo)
+			coinwinner = 0
+			DInfo = (DName, DLevel, DExp, DHp, DMaxHp, DConst, DStr, DIntel, DDex)
+			n = n+1
+		await asyncio.sleep(1)
+		await bot.edit_message(msg,new_content="%s 🗡 Remaining HP : %s \n %s 🛡 Remaining HP : %s" % (AName,AHp , DName, DHp))
+		print(n)
+	if AHp <=0 :
+		winner = DName
+		exp(DInfo,AInfo)
+	else:
+		winner = AName
+		exp(AInfo,DInfo)
 	return winner ,AInfo ,DInfo
+
+def combat(AInfo , DInfo):
+	AStr = AInfo[6]
+	DDex = DInfo[8]
+	DHp = DInfo[3]
+	Dice = random.randint(1, 6)
+	DMG = (AStr + Dice) - DDex
+	if DMG <=0:
+		DMG = 0
+	print("dmg"+str(DMG))
+	DHp -= DMG
+	return DHp
+
+def exp(W,L):
+	WExp = W[2]
+	LExp = L[2]
+	WName = W[0]
+	LName = L[0]
+
+	WExp = WExp+random.randint(9, 11)
+	LExp = LExp+random.randint(4, 6)
+	sqlW = "UPDATE stats SET Exp = %s WHERE Name = '%s'" % (WExp,WName)
+	print(sqlW)
+	cursor.execute(sqlW)
+	sqlL = "UPDATE stats SET Exp = %s WHERE Name = '%s'" % (LExp,LName)
+	print(sqlL)
+	cursor.execute(sqlL)
+	cnx.commit()
 
